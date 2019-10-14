c060c245b40ae6e867df2c695a4c7cea98e33dcd
Daniel Graham <dfgraham@gmail.com>
2019-10-14 19:56:44 -0400

    refactor: Cleanup header import

--- src/components/TabControl.js
+++ src/components/TabControl.js
@@ -3,8 +3,7 @@ import React from 'react';
 import { Button, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faVolumeMute } from '@fortawesome/free-solid-svg-icons'
-import { faVolumeUp } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
 
 const useStyles = makeStyles(() => ({
   container: {
